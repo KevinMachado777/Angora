9d8c894011ec9d8c114e42a8ab8800e15dc92d53
cxjohancx <johanestebanrios11@gmail.com>
2025-05-29 14:35:58 -0500

    Tarjetas del home

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import "../styles/cards.css";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <section>
+                <div className="card">
+                    <h4>Ingresos</h4>
+                    <p>$ 24.000</p>
+
+                </div>
+                <div className="card">
+                    <h4>Utilidad</h4>
+                    <p>$ 25.000</p>
+
+                </div>    
+                <div className="card">
+                    <h4>Cantidad de pedidos</h4>
+                    <p>10</p>
+
+                </div>    
+                <div className="card">
+                    <h4>Ingresos</h4>
+                    <p>$ 15.000</p>
+
+                </div>    
+                <div className="card">
+                    <h4>Ordenes de compra</h4>
+                    <p>4</p>
+
+                </div>    
+                <div className="card">
+                    <h4>Facturas</h4>
+                    <p>7</p>
+
+
+                </div>        
+            </section>
+        </>
+
+        
     )
 }
 
