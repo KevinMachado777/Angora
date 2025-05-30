6e33a7fdd0930a0fcc6bfed812590a64fc1427b8
cxjohancx <johanestebanrios11@gmail.com>
2025-05-30 09:28:17 -0500

    Footer y estilo del footer

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../styles/footer.css"
+const Footer = () => {
+    return (
+        
+        <footer>
+
+            <a href="">Inicio</a>
+            <a href="">Perfil</a>
+            <a href="">Inventario</a>
+            <a href="">Contactos</a>
+            <h4>© 2025 Kase</h4>
+            
+        </footer>
+    )
+}
+
+export default Footer
