770b2e85d399b04ee92975d8627de7b2bcda1378
Johan <johanestebanrios11@gmail.com>
2025-06-19 16:20:43 -0500

    Diseño del perfil v2.0

--- Frontend/src/pages/Perfil.jsx
+++ Frontend/src/pages/Perfil.jsx
@@ -5,26 +5,23 @@ import { Form } from "react-router-dom";
 import productos from "../assets/images/Productos_Perfil.png";
 
 const Perfil = () => {
-  const [Editar, setEditar] = useState(true);
+  const [Editar, setEditar] = useState(false);
+  const [Girar, setGirar] = useState(false);
 
   return (
     <main className="main perfil">
-      <div>
+      <div className="renderizado">
         {Editar ? (
           <div className="formulario">
             <h2>Datos básicos</h2>
-
             <form action="">
               <label htmlFor="correo">Correo</label>
               <input type="email" name="correo" id="correo" />
-
               <label htmlFor="telefono">Teléfono</label>
               <input type="number" name="telefono" id="telefono" />
-
               <label htmlFor="direccion">Dirección</label>
               <input type="text" name="direccion" id="direccion" />
             </form>
-
             <div>
               <button>Guardar</button>
               <button>Cancelar</button>
@@ -42,11 +39,23 @@ const Perfil = () => {
         )}
       </div>
 
-      <div className="tarjeta-perfil">
-        <img src={logoUser} width={"250px"} alt="" />
-        <h3>Rol</h3>
-        <h5>Johan Rios</h5>
-        <button onClick={() => setEditar(!Editar)}>Editar pefil</button>
+      <div className="tarjeta-perfil" onClick={() => setGirar(!Girar)}>
+        <div className={`carta-general ${Girar ? 'girado' : ''}`} >
+          <div className="carta-frontal">
+            <img src={logoUser} width={"150px"} alt="" />
+            <h3>Rol</h3>
+            <h5>Johan Rios</h5>
+           
+          </div>
+          <div className="carta-trasera">
+            <h3>Nombre</h3>
+            <h3>Apellidos</h3>
+            <h3>Correo</h3>
+            <h3>Telefono</h3>
+            <h3>Direccion</h3>
+          </div>
+        </div>
+         <button onClick={(e) => {e.stopPropagation(); setEditar(!Editar)}}>Editar perfil</button>
       </div>
     </main>
   );
