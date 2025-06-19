6925c5df4acbb2b59d57d08e9f82bf1f63492b1e
KevinMachado_dev <kevinandresmachadorueda@gmail.com>
2025-06-19 15:01:42 -0500

    Creacion del componente de ventana modal y personal V2.0

---
+++ Frontend/src/components/Modal.jsx
@@ -0,0 +1,23 @@
+import '../styles/modal.css';
+
+// Componente reutilizable para modales
+const Modal = ({ isOpen, onClose, children }) => {
+    // Si isOpen es falso, no renderiza nada
+    if (!isOpen) return null;
+
+    return (
+        // Fondo oscuro al fondo del modal, cierra el modal al hacer clic fuera
+        <div className="modal-fondo" onClick={onClose}>
+
+            {/* Contenedor del contenido del modal, el onClick 
+            evita que se cierre la ventana al dar click en ella */}
+            <div className="modal-contenido" onClick={(e) => e.stopPropagation()}>
+
+                {/* Contenido hijo para mostrar diferente contenido en la modal*/}
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
