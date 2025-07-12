8bf2b13d903ffd04caa7eab524ce5b3e0873785e
KevinMachado_dev <kevinandresmachadorueda@gmail.com>
2025-07-12 12:43:08 -0500

    Se agregaron unas modificaciones en el App.jsx para evitar el scroll en inputs numericos y de esa misma forma evitar la modificacion de valores

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
 import AppRoutes from './routes/AppRoutes'
+import { useEffect } from 'react';
 
 function App() {
+  // Evita el scroll al usar el mouse wheel en inputs de tipo number
+  // Esto es útil para evitar que el scroll afecte a los inputs numéricos
+  useEffect(() => {
+    const handleWheel = (e) => {
+      if (document.activeElement.type === "number") {
+        e.preventDefault();
+      }
+    };
+
+    // Agrega el listener global
+    document.addEventListener("wheel", handleWheel, { passive: false });
+
+    // Limpia el listener al desmontar
+    return () => {
+      document.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
   return (
     <div>
       <AppRoutes/>
